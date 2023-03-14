f2204d6c4eebc3ecad8a3d2a78c53c3d0f7a1541
liang-heartbeat <zhang@heartbeat-med.de>
2023-03-14 12:43:16 +0100

    action

--- oryInstall.sh
+++ oryInstall.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-bash <(curl https://raw.githubusercontent.com/ory/meta/master/install.sh) -b . ory
-
-sudo mv ./ory /usr/local/bin/
+curl https://raw.githubusercontent.com/ory/meta/master/install.sh | sh -s ory
