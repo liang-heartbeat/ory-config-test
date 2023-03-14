f4602caa707b024d3ac546ea84ea2eff66adfb80
liang-heartbeat <zhang@heartbeat-med.de>
2023-03-14 13:23:32 +0100

    add everything in github action

--- permissionScript.sh
+++ permissionScript.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "step 1: generate config json file"
-
-echo "step 2: push the json file to ory"
-
-ory update permission-config 143f18ba-304b-40d2-8894-f479b1007961 --file ./src/permission-config.json
+ory update permission-config 143f18ba-304b-40d2-8894-f479b1007961 --file ../../src/permission-config.json
 
 rm -- ./src/permission-config.json
